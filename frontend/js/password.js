594510ff95084e81d9da085d7de71147c6ad7751
WinterSoldier13 <ayushsingh1315@gmail.com>
2022-06-11 18:39:48 +0530

    some_commit

--- frontend/js/password.js
+++ frontend/js/password.js
@@ -1,8 +1,16 @@
 function submit_workflow(){
-    alert("if you are going for encrypt operation make sure you rememeber this password, or else you will loose your files forever");
 
-    let password = document.getElementById("password").textContent;
+    let password = document.getElementById("password_field").textContent;
     let re_password = document.getElementById("re_password").textContent;
+
+    alert(password)
+
+    if(password.length == 0 || re_password.length == 0){
+        alert("this")
+        return;
+    }
+
+    alert("if you are going for encrypt operation make sure you rememeber this password, or else you will loose your files forever");
 
     if(password != re_password){
         alert("Passwords do not match!")
