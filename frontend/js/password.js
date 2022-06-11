56add0d77755880081b986c72a5d773ceaa8217e
WinterSoldier13 <ayushsingh1315@gmail.com>
2022-06-11 18:30:34 +0530

    added further workflow

--- frontend/js/password.js
+++ frontend/js/password.js
@@ -1,3 +1,13 @@
 function submit_workflow(){
-    alert("if you are going for encrypt operation make sure you rememeber this password, or else you will loose your files forever")
+    alert("if you are going for encrypt operation make sure you rememeber this password, or else you will loose your files forever");
+
+    let password = document.getElementById("password").textContent;
+    let re_password = document.getElementById("re_password").textContent;
+
+    if(password != re_password){
+        alert("Passwords do not match!")
+    }
+    else{
+        // todo start the backend server and do the encryption
+    }
 }
